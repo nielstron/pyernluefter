9f0c2c5465635083a7e31006e4df9a5116e6a782
Niels Mündler <n.muendler@tum.de>
2020-05-04 00:44:32 +0200

    Small version bump for automatic deploy test

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ short_description = '{}'.format(
 
 setup(
     name='pyernluefter',
-    version='0.0.1',
+    version='0.0.2',
     description=short_description,
     author='nielstron',
     author_email='n.muendler@web.de',
@@ -35,8 +35,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Object Brokering',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
